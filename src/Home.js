5e2ffaae0c02bd3ce9bbc7af6d77a9c3782dc450
Alisson Patricio <eu@alisson.net>
2020-01-22 19:19:49 -0300

    map home

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,17 @@ import Route from './relay/RouteWithLoading';
 import { Grid, Paper, Typography, withStyles } from '@material-ui/core';
 import { Width } from './ui'
 import HomeQuery from './Home.query.js';
-import LatestWhatIsThis from './occurrences/identify/LatestWhatIsThis.js';
-import PostList from './blog/PostList.js';
-import TopPlants from './plants/TopPlants.js';
+// import LatestWhatIsThis from './occurrences/identify/LatestWhatIsThis.js';
+// import PostList from './blog/PostList.js';
+// import TopPlants from './plants/TopPlants.js';
+import {defaultBbox} from './occurrences/Map.js';
+import OccurrencesMap from './occurrences/OccurrencesMap.js';
 
 function Home(props) {
-	const {classes, viewer} = props;
+	const {classes, viewer, environment} = props;
   return <Width component="div">
-		<Grid container spacing={3} alignItems="flex-start">
+    <OccurrencesMap className={classes.map} environment={environment} viewer={viewer} />
+    {/*<Grid container spacing={3} alignItems="flex-start">
 			<Grid item xs={12} sm={6}>
         <Typography component="h3" variant="h5" className={classes.titleWhat}>Últimos pedidos de identificação</Typography>
         <LatestWhatIsThis identified={false} viewer={viewer} environment={props.environment} />
@@ -26,7 +29,7 @@ function Home(props) {
           </Paper>
         </Grid>
 			</Grid>
-		</Grid>
+    </Grid>*/}
 	</Width>
 }
 
@@ -39,12 +42,18 @@ const styles = (theme) => ({
   },
   gridPaddingBottom: {
     paddingBottom: theme.spacing(2),
-  }
+  },
+  map: {
+    height: 500,
+  },
 })
 
 const HomeStyled = withStyles(styles)(Home)
 
 export const homeRoute = <Route
 	query={HomeQuery}
+  prepareVariables={() => ({
+    bbox: defaultBbox,
+  })}
   render={(args) => <HomeStyled {...args.props} environment={args.environment} />}
 />
--- src/Home.query.js
+++ src/Home.query.js
@@ -1,8 +1,10 @@
 import graphql from 'babel-plugin-relay/macro';
 
 const query = graphql`
-  query HomeQuery {
+  query HomeQuery($bbox: String!) {
     viewer {
+      id
+      ...OccurrencesMapConnection_viewer @arguments(bbox: $bbox)
       ...PostList_viewer @arguments(count: 10)
     }
   }
