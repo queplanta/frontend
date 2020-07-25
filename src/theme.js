541795a51a7e80e4da2026f4d60275ef8aab4de4
Alisson Patricio <eu@alisson.net>
2020-07-25 13:01:49 -0300

    add material-ui ptBR translation

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,14 @@
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 import green from "@material-ui/core/colors/green";
+import { ptBR } from "@material-ui/core/locale";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: green,
+const theme = createMuiTheme(
+  {
+    palette: {
+      primary: green,
+    },
   },
-});
+  ptBR
+);
 
 export default responsiveFontSizes(theme);
