21b831082fe403aa29fb552f3e5835eb48372985
Alisson Patricio <eu@alisson.net>
2020-08-21 23:27:03 -0300

    make it center for streets and UI improvements

--- src/occurrences/Map.js
+++ src/occurrences/Map.js
@@ -6,7 +6,9 @@ import {
   FormControl,
   InputLabel,
   FilledInput,
+  OutlinedInput,
   InputAdornment,
+  TextField,
   IconButton,
 } from "@material-ui/core";
 import { Alert, AlertTitle } from "@material-ui/lab";
@@ -30,6 +32,9 @@ let reactLeaflet,
 
 // // export const defaultPosition = [-19.964222, -43.407032];
 export const defaultPosition = [-19.923105, -43.933799];
+
+const MAPBOX_API_KEY =
+  "pk.eyJ1IjoicXVlcGxhbnRhIiwiYSI6ImNrOWRqemM2MzAycHQzaGxsd2RjMm02eGEifQ.hWRMUFwqlSf8nbEiJIE5BQ";
 
 export class Marker extends Component {
   constructor(props) {
@@ -77,14 +82,6 @@ export class Map extends Component {
     this.setState({ isLoading: false });
   }
 
-  componentDidUpdate(prevProps) {
-    if (
-      prevProps.newZoom !== this.props.newZoom &&
-      prevProps.center[0] !== this.props.center[0]
-    )
-      this.setState({ zoom: this.props.newZoom });
-  }
-
   onZoom(e) {
     const { _zoom: zoom } = e.target;
     this.setState({ zoom });
@@ -94,7 +91,7 @@ export class Map extends Component {
     if (!LeafletMap || !LeafletMap | !TileLayer) return null;
 
     const { children, mapRef, ...mapProps } = this.props;
-    const { zoom, viewport } = this.state;
+    const { zoom } = this.state;
 
     return (
       <LeafletMap
@@ -108,146 +105,186 @@ export class Map extends Component {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
       />*/}
-        <TileLayer url="https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicXVlcGxhbnRhIiwiYSI6ImNrOWRqemM2MzAycHQzaGxsd2RjMm02eGEifQ.hWRMUFwqlSf8nbEiJIE5BQ" />
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=${MAPBOX_API_KEY}`}
+        />
         {children}
       </LeafletMap>
     );
   }
 }
 
-export const MapGeolocated = geolocated({
-  userDecisionTimeout: 20000,
-  suppressLocationOnMount: true,
-  isOptimisticGeolocationEnabled: false,
-})(
-  class extends Component {
-    constructor(props) {
-      super(props);
+const mapStyles = (theme) => ({
+  searchForm: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    zIndex: 1000,
+    background: "rgba(0, 80, 39, 0.5)",
+    padding: theme.spacing(1),
+    borderRadius: 4,
+    width: "75%",
+    [theme.breakpoints.up("sm")]: {
+      width: "25%",
+    },
+  },
+  input: {
+    background: "rgba(255, 255, 255, 0.9)",
+    width: "100%",
+  },
+});
 
-      this.state = {
-        position: props.position || defaultPosition,
-        searchValue: "",
-        error: null,
-      };
+export const MapGeolocated = withStyles(mapStyles)(
+  geolocated({
+    userDecisionTimeout: 20000,
+    suppressLocationOnMount: true,
+    isOptimisticGeolocationEnabled: false,
+  })(
+    class extends Component {
+      constructor(props) {
+        super(props);
 
-      this.ref = React.createRef();
-      this.toGoMyLocation = this.toGoMyLocation.bind(this);
-      this.handlerInputSearch = this.handlerInputSearch.bind(this);
-    }
+        this.state = {
+          position: props.position || defaultPosition,
+          searchValue: "",
+          error: null,
+        };
 
-    componentDidUpdate(prevProps, prevState) {
-      const { coords, onPositionChange } = this.props;
-      const { position, error, searchValue } = this.state;
-      if (
-        position !== prevState.position &&
-        typeof onPositionChange === "function"
-      ) {
-        onPositionChange(position);
+        this.ref = React.createRef();
+        this.toGoMyLocation = this.toGoMyLocation.bind(this);
+        this.handlerInputSearch = this.handlerInputSearch.bind(this);
       }
 
-      if (error && error.triedSearchValue !== searchValue) {
-        this.setState({ error: null });
+      componentDidUpdate(prevProps, prevState) {
+        const { coords, onPositionChange } = this.props;
+        const { position, error, searchValue } = this.state;
+        if (
+          position !== prevState.position &&
+          typeof onPositionChange === "function"
+        ) {
+          onPositionChange(position);
+        }
+
+        if (error && error.triedSearchValue !== searchValue) {
+          this.setState({ error: null });
+        }
+
+        if (coords !== prevProps.coords) {
+          this.setState({ position: [coords.latitude, coords.longitude] });
+        }
       }
 
-      if (coords !== prevProps.coords) {
-        this.setState({ position: [coords.latitude, coords.longitude] });
+      handlerInputSearch(event) {
+        this.setState({ searchValue: event.target.value });
       }
-    }
 
-    handlerInputSearch(event) {
-      this.setState({ searchValue: event.target.value });
-    }
+      getZoomByType(placeType) {
+        switch (placeType) {
+          case "country":
+            return 5;
+          case "region":
+            return 8;
+          case "district":
+            return 10;
+          case "place":
+            return 14;
+          case "locality":
+            return 18;
+          case "neighborhood":
+            return 24;
+          case "address":
+            return 30;
+          case "poi":
+            return 40;
+          default:
+            return 14;
+        }
+      }
 
-    getDataAPI() {
-      const { searchValue } = this.state;
-      const MAPBOX_API_KEY =
-        "pk.eyJ1IjoicXVlcGxhbnRhIiwiYSI6ImNrOWRqemM2MzAycHQzaGxsd2RjMm02eGEifQ.hWRMUFwqlSf8nbEiJIE5BQ";
-      if (searchValue.length) {
-        fetch(
-          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchValue}.json?access_token=${MAPBOX_API_KEY}&fuzzyMatch=false&language=pt-BR`
-        )
-          .then((data) => data.json())
-          .then((json) => {
-            if (
-              json.features &&
-              json.features.length &&
-              json.features[0].bbox
-            ) {
-              const location = json.features[0];
-              this.ref.current.leafletElement.fitBounds([
-                [location.bbox[1], location.bbox[0]],
-                [location.bbox[3], location.bbox[2]],
-              ]);
-            } else {
-              this.setState({
-                error: {
-                  msg: "Nenhum resultado encontrado!",
-                  triedSearchValue: searchValue,
-                },
-              });
+      getDataAPI() {
+        const { searchValue } = this.state;
+        if (searchValue.length) {
+          this.setState(
+            {
+              error: null,
+            },
+            () => {
+              fetch(
+                `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchValue}.json?access_token=${MAPBOX_API_KEY}&fuzzyMatch=false&language=pt-BR`
+              )
+                .then((data) => data.json())
+                .then((json) => {
+                  if (json.features && json.features.length) {
+                    const location = json.features[0];
+                    if (location.bbox) {
+                      this.ref.current.leafletElement.fitBounds([
+                        [location.bbox[1], location.bbox[0]],
+                        [location.bbox[3], location.bbox[2]],
+                      ]);
+                    } else {
+                      this.ref.current.leafletElement.setView(
+                        [location.center[1], location.center[0]],
+                        this.getZoomByType(location.place_type)
+                      );
+                    }
+                  } else {
+                    this.setState({
+                      error: {
+                        msg: "Nenhum resultado encontrado!",
+                        triedSearchValue: searchValue,
+                      },
+                    });
+                  }
+                });
             }
+          );
+        } else {
+          this.setState({
+            error: {
+              msg: "O campo de busca não pode estar vazio",
+              triedSearchValue: searchValue,
+            },
           });
-      } else {
-        this.setState({
-          error: {
-            msg: "O campo de busca não pode estar vazio",
-            triedSearchValue: searchValue,
-          },
-        });
+        }
       }
-    }
 
-    toGoMyLocation(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      this.setState({ zoom: 14 });
-    }
+      toGoMyLocation(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({ zoom: 14 });
+      }
 
-    render() {
-      const { children, ...mapProps } = this.props;
-      const { position, searchValue, error } = this.state;
+      render() {
+        const { classes, children, ...mapProps } = this.props;
+        const { position, searchValue, error } = this.state;
 
-      return (
-        <Map mapRef={this.ref} center={position} {...mapProps}>
-          <form
-            style={{
-              position: "absolute",
-              width: "25%",
-              top: 10,
-              right: 10,
-              zIndex: 1000,
-            }}
-            autoComplete="off"
-            onClick={(e) => e.stopPropagation()}
-            onDoubleClick={(e) => e.stopPropagation()}
-            onSubmit={(e) => {
-              e.preventDefault();
-              this.getDataAPI();
-            }}
-          >
-            <FormControl
-              variant="filled"
-              style={{ width: "100%" }}
-              error={!!error}
-              onDoubleClick={(e) => e.stopPropagation()}
+        return (
+          <Map mapRef={this.ref} center={position} {...mapProps}>
+            <form
+              className={classes.searchForm}
+              autoComplete="off"
+              onSubmit={(e) => {
+                e.preventDefault();
+                this.getDataAPI();
+              }}
             >
-              <InputLabel style={{ fontSize: "14px" }} htmlFor="input-map">
-                Buscar por endereço
-              </InputLabel>
-              <FilledInput
-                id="input-map"
+              <TextField
+                style={{ width: "100%" }}
+                placeholder="Buscar por endereço"
                 value={searchValue}
-                style={{ backgroundColor: "rgba(255,255,255,0.65)" }}
-                onClick={(e) => e.stopPropagation()}
-                onDoubleClick={(e) => e.stopPropagation()}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton type="submit" edge="end">
-                      <SearchIcon />
-                    </IconButton>
-                  </InputAdornment>
-                }
+                InputProps={{
+                  className: classes.input,
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton type="submit" size="small">
+                        <SearchIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
                 onChange={(event) => this.handlerInputSearch(event)}
+                size="small"
+                variant="outlined"
               />
               {error && (
                 <Alert
@@ -261,22 +298,27 @@ export const MapGeolocated = geolocated({
                   {error.msg}
                 </Alert>
               )}
-            </FormControl>
-          </form>
-          {children}
-          <Tooltip title="Minha localização atual" placement="top">
-            <MapButton
-              variant="outlined"
-              style={{ position: "absolute", top: 80, left: 10, zIndex: 1000 }}
-              onClick={this.toGoMyLocation}
-            >
-              <MyLocationIcon />
-            </MapButton>
-          </Tooltip>
-        </Map>
-      );
+            </form>
+            {children}
+            <Tooltip title="Minha localização atual" placement="top">
+              <MapButton
+                variant="outlined"
+                style={{
+                  position: "absolute",
+                  top: 80,
+                  left: 10,
+                  zIndex: 1000,
+                }}
+                onClick={this.toGoMyLocation}
+              >
+                <MyLocationIcon />
+              </MapButton>
+            </Tooltip>
+          </Map>
+        );
+      }
     }
-  }
+  )
 );
 
 export class Popup extends Component {
